7e54c9e28078e24e233710443f9b99ed8f9e259e
Fahad <113378312+Fahad-shi@users.noreply.github.com>
2023-04-14 01:56:05 +0300

    added insert coomand

--- dags/sql_queries.py
+++ dags/sql_queries.py
@@ -1,118 +1,51 @@
-import configparser
-
-
-# CONFIG
-config = configparser.ConfigParser()
-config.read('dwh.cfg')
-
-
 
 # DROP TABLES if existed
 
 staging_surf_report_table_drop = "DROP table IF EXISTS staging_surf_report"
-staging_tides_table_drop = "DROP table IF EXISTS staging_tides"
+tides_table_drop = "DROP table IF EXISTS tides"
 
 # CREATE TABLES
 
-staging_surf_report_table_create= ("""CREATE TABLE IF NOT EXISTS staging_events(
-                                artist varchar,
-                                auth varchar,
-                                firstName varchar,
-                                gender varchar,
-                                itemInSession int,
-                                lastName varchar,
-                                length float,
-                                level varchar,
-                                location varchar,
-                                method varchar,
-                                page varchar,
-                                registration float,
-                                sessionId int,
-                                song varchar,
-                                status int,
-                                ts timestamp,
-                                userAgent varchar,
-                                userId int
+staging_surf_report_table_create = ("""CREATE TABLE IF NOT EXISTS staging_surf_report(
+                                timestamp TIMESTAMP PRIMARY KEY,
+                                surf_min INTEGER,
+                                surf_max INTEGER,
+                                surf_optimalScore INTEGER,
+                                surf_plus BOOL,
+                                surf_humanRelation VARCHAR(255),
+                                surf_raw_min NUMERIC,
+                                surf_raw_max NUMERIC,
+                                speed NUMERIC,
+                                direction NUMERIC,
+                                directionType VARCHAR(255),
+                                gust NUMERIC,
+                                optimalScore INTEGER,
+                                temperature NUMERIC,
+                                condition VARCHAR(255)
+                        )
+                """)
+
+tides_table_create = ("""CREATE TABLE IF NOT EXISTS tides(
+                                tide_timestamp TIMESTAMP PRIMARY KEY,
+                                tide_type VARCHAR(255),
+                                height NUMERIC
     )
 """)
-
-staging_tides_table_create = ("""CREATE TABLE IF NOT EXISTS staging_songs(
-                                num_songs int,
-                                artist_id varchar,
-                                artist_latitude float,
-                                artist_longitude float,
-                                artist_location varchar,
-                                artist_name varchar,
-                                song_id varchar,
-                                title varchar,
-                                duration float,
-                                year int
-    )
-""")
-
-# load the from the API to s3
-
-staging_surf_report_table_copy = (
-    """
-    COPY staging_events
-    FROM {} 
-    iam_role {}
-    JSON {}
-    REGION 'us-west-2'
-    TIMEFORMAT as 'epochmillisecs';
-""").format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'], config['S3']['LOG_JSONPATH'])
-
-staging_tides_table_copy = (
-    """
-    COPY staging_songs
-    FROM {} 
-    iam_role {}
-    FORMAT AS JSON 'auto'
-    REGION 'us-west-2';
-""").format(config['S3']['SONG_DATA'], config['IAM_ROLE']['ARN'])
-
-
-# STAGING TABLES from the s3 to s3 after analysis
-
-staging_surf_report_table_copy = (
-    """
-    COPY staging_events
-    FROM {} 
-    iam_role {}
-    JSON {}
-    REGION 'us-west-2'
-    TIMEFORMAT as 'epochmillisecs';
-""").format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'], config['S3']['LOG_JSONPATH'])
-
-staging_tides_table_copy = (
-    """
-    COPY staging_songs
-    FROM {} 
-    iam_role {}
-    FORMAT AS JSON 'auto'
-    REGION 'us-west-2';
-""").format(config['S3']['SONG_DATA'], config['IAM_ROLE']['ARN'])
-
-
 
 
 # FINAL TABLES
 
-songplay_table_insert = ("""INSERT INTO fact_songplay(start_time, user_id,level, song_id, artist_id, session_id, location, user_agent)
-                                SELECT DISTINCT ste.ts, ste.userId , ste.level,
-                                sts.song_id,sts.artist_id,ste.sessionId,
-                                sts.artist_location,ste.userAgent 
-                                FROM staging_events ste 
-                                JOIN staging_songs sts
-                                ON ste.song = sts.title AND ste.length = sts.duration 
-                                AND ste.artist = sts.artist_name;
-                       
-                        
-""")
+surf_report_insert = ("""
+        INSERT INTO surf_report
+      (timestamp, surf_min,surf_max,surf_optimalScore,surf_plus,surf_humanRelation,surf_raw_min,surf_raw_max,speed,direction,directionType,gust,optimalScore,temperature,condition)
+        SELECT *
+    FROM staging_surf_report
+        WHERE NOT EXISTS(SELECT timestamp
+                    FROM surf_report 
+                   WHERE staging_surf_report.timestamp = surf_report.timestamp);""")
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_tides_table_create]
-drop_table_queries = [staging_events_table_drop, staging_tides_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert]
+create_table_queries = [staging_surf_report_table_create, tides_table_create]
+drop_table_queries = [staging_surf_report_table_drop, tides_table_drop]
+insert_table_queries = [surf_report_insert]
